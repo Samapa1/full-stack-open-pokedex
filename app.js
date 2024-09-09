5e97893701bc22668bc255d9c9bee383976fb00c
Samapa1 <saija.m.pajari@gmail.com>
2024-09-09 14:25:04 +0300

    11.12

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('health check ok')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
 })
+
+// app.get('/health', (req, res) => {
+//   res.send('health check ok')
+// })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
